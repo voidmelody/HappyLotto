5f7ea99c883bfe67125f5d812976cbc9201a53b5
voidmelody <sss9073@naver.com>
2023-11-02 21:53:34 +0900

    setting: "초기 환경 설정"

---
+++ src/main/java/com/programmers/happylotto/HappyLottoApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.happylotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyLottoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyLottoApplication.class, args);
+    }
+
+}
