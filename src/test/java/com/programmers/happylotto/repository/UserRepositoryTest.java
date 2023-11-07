df2f94c2fb6bff9d4e095b1c883fd22a7fe369e6
voidmelody <sss9073@naver.com>
2023-11-07 13:23:04 +0900

    test: "유저 이름과 이메일을 통한 유저 조회 테스트 구현"

--- src/test/java/com/programmers/happylotto/repository/UserRepositoryTest.java
+++ src/test/java/com/programmers/happylotto/repository/UserRepositoryTest.java
@@ -36,14 +36,56 @@ class UserRepositoryTest {
                 .build();
         userRepository.save(user);
 
-        //when
-        userRepository.save(user);
-
-        //then
+        //when //then
         Optional<User> foundUserOptional = userRepository.findByUsernameAndEmail(targetUsername, targetEmail);
 
         assertThat(foundUserOptional)
                 .isPresent()
                 .contains(user);
+    }
+
+    @DisplayName("존재하지 않은 이름을 통해서는 유저를 조회할 수 없다.")
+    @Test
+    void findByInvalidUsernameAndEmail(){
+        //given
+        String targetEmail = "test1234@gmail.com";
+        String targetUsername = "test봇";
+        String invalidUsername = "테스트봇";
+        User user = User.builder()
+                .userId(UUID.randomUUID())
+                .username(targetUsername)
+                .email(targetEmail)
+                .createdAt(LocalDateTime.now())
+                .build();
+        userRepository.save(user);
+
+        //when //then
+        Optional<User> foundInvalidUserOptional = userRepository.findByUsernameAndEmail(invalidUsername, targetEmail);
+
+        assertThat(foundInvalidUserOptional)
+                .isEmpty();
+     }
+
+    @DisplayName("존재하지 않은 이메일을 통해서는 유저를 조회할 수 없다.")
+    @Test
+    void findByUsernameAndInvalidEmail(){
+        //given
+        String targetEmail = "test1234@gmail.com";
+        String invalidEmail = "test1234@naver.com";
+        String targetUsername = "test봇";
+
+        User user = User.builder()
+                .userId(UUID.randomUUID())
+                .username(targetUsername)
+                .email(targetEmail)
+                .createdAt(LocalDateTime.now())
+                .build();
+        userRepository.save(user);
+
+        //when //then
+        Optional<User> foundInvalidUserOptional = userRepository.findByUsernameAndEmail(targetUsername, invalidEmail);
+
+        assertThat(foundInvalidUserOptional)
+                .isEmpty();
     }
 }
